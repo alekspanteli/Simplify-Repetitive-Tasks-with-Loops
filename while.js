122f506e6534583b5a19abe9fca80235a4a26d70
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2022-09-14 12:21:38 +0300

    1 file modified

--- while.js
+++ while.js
@@ -1,3 +1,21 @@
-function randNum(upper){
-  return Math.floor(Math.random() * upper) + 1;
+// function randNum(upper){
+//   return Math.floor(Math.random() * upper) + 1;
+// }
+
+
+// let counter = 0;
+
+// while (counter < 100){
+//   counter +=1;
+//   console.log(counter);
+// }
+
+
+let counter = 0;
+
+do {
+  counter +=1;
+  console.log(counter);
 }
+
+while (counter < 10);
